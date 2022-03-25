dbef2012f96e33b092065921f60aeaf57aaa5393
Roberto Ruffinengo <ruffinengo@itoa.it>
2022-03-25 18:51:48 +0100

    migration for forgotten Area scientifica

---
+++ config/Migrations/20220325184810_AddGuestsEducationalQualifications.php
@@ -0,0 +1,36 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddGuestsEducationalQualifications extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('guests_educational_qualifications');
+        
+
+        $rows = [
+           
+          
+           
+            [
+                'id' => 13,
+                'name' => 'Area scientifica ',
+                'ordering' => 75,
+                'parent' => 6,
+                'have_children' => false,
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s')
+            ]
+          
+          
+        ];
+        $this->insert('guests_educational_qualifications', $rows);
+    }
+}
