eab4bc1b65e585ea36a1c82066c3012291846ced
Matthew Grill <m.grill@itoa.it>
2022-04-08 16:00:15 +0200

    fix lista ospiti per stato

--- plugins/Aziende/src/Model/Table/GuestsFamiliesTable.php
+++ plugins/Aziende/src/Model/Table/GuestsFamiliesTable.php
@@ -86,7 +86,7 @@ class GuestsFamiliesTable extends Table
             'GuestsFamilies.family_id' => $familyId,
             'GuestsFamilies.guest_id !=' => $guestId,
             'g.sede_id' => $sedeId,
-            'g.status_id IN' => [1, 2, 3, 5],
+            'g.status_id !=' => 6,
             'g.deleted' => '0'
         ];
 
