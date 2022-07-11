ecf22d1a257f8eba21d5ef5bf6aeef626fd740be
Stevie <stefano.de.simone@itoa.it>
2022-07-11 17:24:28 +0200

    #18576 migrations

---
+++ config/Migrations/20220711072327_AddCompanyIdToAgreementsToSedi.php
@@ -0,0 +1,23 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddCompanyIdToAgreementsToSedi extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('agreements_to_sedi');
+        $table->addColumn('agreement_company_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->update();
+    }
+}
