1c5d540e4d4b393120e042272453143a2e895eb0
Stevie <stefano.de.simone@itoa.it>
2022-07-22 14:18:41 +0200

    #18761 new form template

--- plugins/Aziende/config/aziendeConfig.php
+++ plugins/Aziende/config/aziendeConfig.php
@@ -25,9 +25,28 @@ return [
                     'contatti_ruoli' => 'Ruoli dei contatti',
                     'sedi_tipi' => 'Tipi di strutture',
                     'aziende_gruppi' => 'Gruppi Enti',
-                    'guests_exit_types' => 'Tipologie uscite ospiti'
+                    'guests_exit_types' => 'Tipologie uscite ospiti',
+                    'periods' => 'Periodi',
+                    'costs_categories' => 'Categorie dei costi'
                 ]
             ]
+        ],
+        'formTemplate' => [
+            'input' => '<input type="{{type}}" name="{{name}}"{{attrs}} class="form-control"/>',
+
+            'select' => '<select class="form-control" name="{{name}}" {{attrs}} /> {{content}} </select>',
+
+            'inputContainer' => 
+                '<div class="form-group">
+                    {{content}}
+                </div>',
+
+            'formGroup' => '{{label}} <div class="col-sm-10"> {{input}} </div>',
+
+            'selectContainer' => 
+                '<div class="form-group">
+                    {{content}}
+                </div>',
         ]
     ]
 ];
