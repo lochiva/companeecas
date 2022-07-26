5ad25d19973646bb4e639201c7681beb473d853e
Stevie <stefano.de.simone@itoa.it>
2022-07-26 10:02:39 +0200

    #18761 create default for periods and status

---
+++ config/Migrations/20220726074137_InsertStatus.php
@@ -0,0 +1,25 @@
+<?php
+use Migrations\AbstractMigration;
+
+class InsertStatus extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $rows = [
+            [
+                'id' => 1,
+                'name' => 'In corso',
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s')
+            ]
+        ];
+        $this->insert('status', $rows);
+    }
+}
---
+++ config/Migrations/20220726074143_InsertPeriods.php
@@ -0,0 +1,26 @@
+<?php
+use Migrations\AbstractMigration;
+
+class InsertPeriods extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $rows = [
+            [
+                'id' => 1,
+                'label' => 'Personalizzato',
+                'visible' => 1,
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s')
+            ]
+        ];
+        $this->insert('periods', $rows);
+    }
+}
