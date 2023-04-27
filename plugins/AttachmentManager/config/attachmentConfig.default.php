76e05ed9290b3fe4f22c311692528d3273151e50
Roberto Ruffinengo <ruffinengo@itoa.it>
2023-04-27 11:40:31 +0200

    prima versione license

--- plugins/AttachmentManager/config/attachmentConfig.default.php
+++ plugins/AttachmentManager/config/attachmentConfig.default.php
@@ -1,4 +1,24 @@
 <?php
+
+################################################################################
+#
+# Attachment manager is a plugin to manage attachment
+#
+# Companee :    Attachement manager  (https://www.companee.it)
+# Copyright (c) lochiva , (http://www.lochiva.it)
+#
+# Licensed under The GPL  License
+# For full copyright and license information, please see the LICENSE.txt
+# Redistributions of files must retain the above copyright notice.
+#
+# @copyright     Copyright (c) LOCHIVA , (httpS://www.lochiva.com)
+# @link          https://www.companee.it Companee project
+# @since         1.2.0
+# @license       http://www.opensource.org/licenses/mit-license.php MIT License
+#
+################################################################################
+
+
 return [
     /*
      * Plugin configurations
