4e77ead0c22c69a271ec765a7579c5b5596b0225
Renato Cau <renato-stage@itoa.it>
2023-05-09 09:25:58 +0200

    20579 fix 2

--- src/Model/Table/ActionLogTable.php
+++ src/Model/Table/ActionLogTable.php
@@ -1,21 +1,19 @@
 <?php
-
+/** 
+* Companee :    ActionLog (https://www.companee.it)
+* Copyright (c) lochiva , (http://www.lochiva.it)
+*
+* Licensed under The GPL  License
+* For full copyright and license information, please see the LICENSE.txt
+* Redistributions of files must retain the above copyright notice.
+*
+* @copyright     Copyright (c) LOCHIVA , (https://www.lochiva.com)
+* @link          https://www.companee.it Companee project
+* @since         1.2.0
+* @license       https://www.gnu.org/licenses/gpl-3.0.html GPL 3
+*/
 namespace App\Model\Table;
-################################################################################
-#
-# Companee :   ActionLog (https://www.companee.it)
-# Copyright (c) lochiva , (http://www.lochiva.it)
-#
-# Licensed under The GPL  License
-# For full copyright and license information, please see the LICENSE.txt
-# Redistributions of files must retain the above copyright notice.
-#
-# @copyright     Copyright (c) LOCHIVA , (https://www.lochiva.com)
-# @link          https://www.companee.it Companee project
-# @since         1.2.0
-# @license       https://www.gnu.org/licenses/gpl-3.0.html GPL 3
-#
-################################################################################
+
 
 use Cake\ORM\Table;
 use Cake\Core\Configure;
